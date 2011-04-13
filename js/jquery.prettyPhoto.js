de71ee5a1dc452750dfe06c823f623e00aefc798
Stephane Caron <scaron@Stephane-Carons-MacBook-Pro.local>
2011-04-13 18:59:50 -0400

    Implement social sharing in prettyPhoto, fix IE7 overlay_gallery overlay bug, fix bug when opening percent based media then opening normal sized media

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -56,6 +56,7 @@
 												<a href="#" class="pp_arrow_next">Next</a> \
 											</div> \
 											<p class="pp_description"></p> \
+											{pp_social} \
 											<a class="pp_close" href="#">Close</a> \
 										</div> \
 									</div> \
@@ -84,7 +85,8 @@
 			quicktime_markup: '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" height="{height}" width="{width}"><param name="src" value="{path}"><param name="autoplay" value="{autoplay}"><param name="type" value="video/quicktime"><embed src="{path}" height="{height}" width="{width}" autoplay="{autoplay}" type="video/quicktime" pluginspage="http://www.apple.com/quicktime/download/"></embed></object>',
 			iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="no"></iframe>',
 			inline_markup: '<div class="pp_inline">{content}</div>',
-			custom_markup: ''
+			custom_markup: '',
+			social_tools: '<div class="pp_social"><div class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div><div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div>'
 		}, pp_settings);
 		
 		// Global variables accessible only by prettyPhoto
@@ -198,13 +200,18 @@
 			$pp_pic_holder.find('.currentTextHolder').text((set_position+1) + settings.counter_separator_label + $(pp_images).size());
 
 			// Set the description
-			$pp_pic_holder.find('.pp_description').show().html(unescape(pp_descriptions[set_position]));
+			if(pp_descriptions[set_position] != ""){
+				$pp_pic_holder.find('.pp_description').show().html(unescape(pp_descriptions[set_position]));
+			}else{
+				$pp_pic_holder.find('.pp_description').hide();
+			}
 			
 			// Get the dimensions
 			movie_width = ( parseFloat(getParam('width',pp_images[set_position])) ) ? getParam('width',pp_images[set_position]) : settings.default_width.toString();
 			movie_height = ( parseFloat(getParam('height',pp_images[set_position])) ) ? getParam('height',pp_images[set_position]) : settings.default_height.toString();
 			
 			// If the size is % based, calculate according to window dimensions
+			percentBased=false;
 			if(movie_height.indexOf('%') != -1) { movie_height = parseFloat(($(window).height() * parseFloat(movie_height) / 100) - 150); percentBased = true; }
 			if(movie_width.indexOf('%') != -1) { movie_width = parseFloat(($(window).width() * parseFloat(movie_width) / 100) - 150); percentBased = true; }
 			
@@ -708,6 +715,9 @@
 	
 		function _build_overlay(caller){
 			
+			if(settings.social_tools)
+				settings.markup = settings.markup.replace('{pp_social}',settings.social_tools);
+			
 			$('body').append(settings.markup); // Inject the markup
 			
 			$pp_pic_holder = $('.pp_pic_holder') , $ppt = $('.ppt'), $pp_overlay = $('div.pp_overlay'); // Set my global selectors
@@ -839,7 +849,7 @@
 	function getHashtag(){
 		url = location.href;
 		hashtag = (url.indexOf('#!') > 0) ? url.substring(url.indexOf('#!')+2,url.length) : false;
-		return hashtag;
+		return decodeURI(hashtag);
 	};
 	
 	function setHashtag(){

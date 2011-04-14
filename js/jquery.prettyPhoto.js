49bf718c052f36e8aba6ad25973a75ce0bace8e2
Stephane Caron <scaron@Stephane-Carons-MacBook-Pro.local>
2011-04-14 08:33:22 -0400

    Fix social tools markup when setting is set to flase

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -86,7 +86,7 @@
 			iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="no"></iframe>',
 			inline_markup: '<div class="pp_inline">{content}</div>',
 			custom_markup: '',
-			social_tools: '<div class="pp_social"><div class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div><div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div>'
+			social_tools: '<div class="pp_social"><div class="twitter"><a href="http://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div><div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div>' /* html or false to disable */
 		}, pp_settings);
 		
 		// Global variables accessible only by prettyPhoto
@@ -715,8 +715,7 @@
 	
 		function _build_overlay(caller){
 			
-			if(settings.social_tools)
-				settings.markup = settings.markup.replace('{pp_social}',settings.social_tools);
+			settings.markup=settings.markup.replace('{pp_social}',(settings.social_tools)?settings.social_tools:'');
 			
 			$('body').append(settings.markup); // Inject the markup
 			

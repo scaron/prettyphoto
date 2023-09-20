f5c17342e873e04a3738eb718bd2c44accf24617
Daniel Bartoníček <xdanik@users.noreply.github.com>
2023-09-20 12:38:53 +0100

    Replace usages of $.size() to add jQuery 3 support

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -190,7 +190,7 @@
 			
 			if(settings.hideflash) $('object,embed,iframe[src*=youtube],iframe[src*=vimeo]').css('visibility','hidden'); // Hide the flash
 
-			_checkPosition($(pp_images).size()); // Hide the next/previous links if on first or last images.
+			_checkPosition($(pp_images).length); // Hide the next/previous links if on first or last images.
 		
 			$('.pp_loaderIcon').show();
 		
@@ -208,7 +208,7 @@
 			$pp_overlay.show().fadeTo(settings.animation_speed,settings.opacity);
 
 			// Display the current position
-			$pp_pic_holder.find('.currentTextHolder').text((set_position+1) + settings.counter_separator_label + $(pp_images).size());
+			$pp_pic_holder.find('.currentTextHolder').text((set_position+1) + settings.counter_separator_label + $(pp_images).length);
 
 			// Set the description
 			if(typeof pp_descriptions[set_position] != 'undefined' && pp_descriptions[set_position] != ""){
@@ -241,7 +241,7 @@
 
 						// Preload the neighbour images
 						nextImage = new Image();
-						if(isSet && set_position < $(pp_images).size() -1) nextImage.src = pp_images[set_position + 1];
+						if(isSet && set_position < $(pp_images).length -1) nextImage.src = pp_images[set_position + 1];
 						prevImage = new Image();
 						if(isSet && pp_images[set_position - 1]) prevImage.src = pp_images[set_position - 1];
 
@@ -382,10 +382,10 @@
 			
 			if(direction == 'previous') {
 				set_position--;
-				if (set_position < 0) set_position = $(pp_images).size()-1;
+				if (set_position < 0) set_position = $(pp_images).length-1;
 			}else if(direction == 'next'){
 				set_position++;
-				if(set_position > $(pp_images).size()-1) set_position = 0;
+				if(set_position > $(pp_images).length-1) set_position = 0;
 			}else{
 				set_position=direction;
 			};
